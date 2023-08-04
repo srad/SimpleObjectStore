a5b105b4ff1568abfc01b432ff5cb0cdc4fcb804
srad <ssedighi@posteo.de>
2023-08-04 18:51:39 +0200

    Use standard claim name for roles.

--- SimpleObjectStore/Models/DTO/CreateStorageFileResult.cs
+++ SimpleObjectStore/Models/DTO/CreateStorageFileResult.cs
@@ -3,6 +3,7 @@
 public class CreateStorageFileResult
 {
     public string FileName { get; set; }
+    public string Url { get; set; }
     public BucketFile? StorageFile { get; set; }
     public bool Success { get; set; }
     public string? ErrorMessage { get; set; }
